34fc827e5a63122289251badfce2cd6bad309d48
harshasv <surahvardhan@gmail.com>
2024-06-30 10:04:07 +0530

    basic slices and for loop

--- Function/function.go
+++ Function/function.go
@@ -15,3 +15,21 @@ func Hello(name string, age int) (string, error) {
 	message := fmt.Sprintf("Hello my name is %v and my age is %v", name, age)
 	return message, nil
 }
+
+func Hellos(names []string, ages []int) (map[string]string, error) {
+	var messages = make(map[string]string)
+	for i := 0; i < len(names); i++ {
+		message, err := Hello(names[i], ages[i])
+		if err != nil {
+			return nil, err
+		}
+		messages[names[i]] = message
+	}
+	return messages, nil
+
+	//a simple for loop for range is as
+	// for _, name := range names {
+
+	// }
+
+}
--- Hello/hello.go
+++ Hello/hello.go
@@ -13,12 +13,17 @@ func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Opt())
-	message, err := function.Hello("", 0)
+	message, err := function.Hello("Harsha", 22)
 
-	if err != nil {
+	names := []string{"Harsha_1", "Harsha_2", "Harsha_3"}
+	ages := []int{22, 22, 22}
+	messages, errs := function.Hellos(names, ages)
+	if err != nil || errs != nil {
 		log.Fatal(err)
+		log.Fatal(errs)
 	} else {
 		fmt.Println(message)
+		fmt.Println(messages)
 	}
 
 }
